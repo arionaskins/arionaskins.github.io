6f1efd9f40cba00d1cbdd355421644540caee783
arionaskins11 <askins1@github.com>
2021-03-19 23:11:53 -0400

    ++ footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div> social links (?) </div>
+            <p>Designed in react (self-taught) by me :)</p> 
+            <p>2021</p>
+        </footer>
+    )
+}
+
+export default Footer;
