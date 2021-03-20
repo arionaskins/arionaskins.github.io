d92633bcb1f39af5578830aca9e02856477368aa
arionaskins11 <askins1@github.com>
2021-03-19 23:10:06 -0400

    import final pages and footer component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
+// import pages and components to be rendered to screen
 import './styles/App.css';
 import NavBar from './components/NavBar/NavBar.js';
 import Main from './pages/main.js'
 import About from './pages/about.js';
 import Strengths from './pages/whatIdo.js';
-import Projects from './pages/projects.js';
+import Projects from './pages/projects/Projects.js';
 import Contact from './pages/contact.js';
+import Footer from './components/Footer.js'
 
 
 function App() {
   return (
     <div className="App">
-      
-      {/* Pages */}
       <NavBar />
+
+      {/* Pages */}
       <Main />
       <About />
       <Strengths />
@@ -21,6 +23,7 @@ function App() {
       <br />
       <Contact />
       
+      <Footer />
 
     </div>
   );
