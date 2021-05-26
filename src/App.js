fc91fdb873f40472992169e089234c1428bc92ce
arionaskins11 <askins1@umd.edu>
2021-05-26 13:49:51 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -30,21 +30,7 @@ function App() {
       <Contact />
       <br /> 
       <Footer />
-
-      {/* fixed scroll to top arrow */}
-      {/* // When the user scrolls down 20px from the top of the document, show the button
-               window.onscroll = function() {scrollFunction()};                  
-               function scrollFunction() {           
-               
-                }                  
-                // When the user clicks on the button, scroll to the top of the document         
-                function topFunction() {          
-                 document.body.scrollTop = 0;           
-      document.documentElement.scrollTop = 0;      
-
-         }          */}
          
-      {/* <a href='#main' className='arrow'><img alt='arrow up' id='arrow' src={icons.arrow}></img></a> */}
         </div>
          <ScrollUpButton 
             StopPosition={0}
